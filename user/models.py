fbdaf24cebab2aa22b17968b1043a04b84d4b067
kimassa <yeswemadeitfinally@gmail.com>
2019-05-06 17:45:43 +0900

    add signup endpoint

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_email = models.CharField(max_length=50)
+    user_nickname = models.CharField(max_length=20)
+    user_password = models.CharField(max_length=20)
