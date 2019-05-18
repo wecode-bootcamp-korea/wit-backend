44380ddeadac696d56b1a419b31cfdb50c31f6b7
Ubuntu <ubuntu@ip-172-31-26-183.ap-northeast-2.compute.internal>
2019-05-18 11:13:38 +0000

    upload

--- wit_backend/settings.py
+++ wit_backend/settings.py
@@ -25,7 +25,7 @@ wit_secret = '4WaxCQFDSDbViX2AvriszHAYYjxpE9al'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
